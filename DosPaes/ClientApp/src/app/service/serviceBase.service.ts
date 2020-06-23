1759386ee2c50341a80926245095d302fad002b9
gustavo <gustavogiori22@hotmail.com>
2020-06-23 15:37:00 -0300

    Ajustando erro de publicação

--- DosPaes/ClientApp/src/app/service/serviceBase.service.ts
+++ DosPaes/ClientApp/src/app/service/serviceBase.service.ts
@@ -1,10 +1,6 @@
-import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 
-@Injectable({
-  providedIn: "root",
-})
 export class ServiceBaseService {
   getAll(): Observable<any[]> {
     return this.http.get<any[]>(this.baseUrl);
